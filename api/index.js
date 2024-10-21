0cefb8c057a1bcfd25fae32838134ad3618d7831
ismailovakamilla193@gmail.com <ismailovakamilla193@gmail.com>
2024-10-21 21:30:37 +0300

    настройка backend

---
+++ api/index.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3010;
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin-dev', '*');
+    next();
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on ${PORT}`);
+});
