b655141b52439b542ba2b97bccf96de4bf201dfc
ismailovakamilla193@gmail.com <ismailovakamilla193@gmail.com>
2024-10-31 18:20:08 +0300

    Styles

--- client/src/Components/header/Header.jsx
+++ client/src/Components/header/Header.jsx
@@ -6,24 +6,26 @@ export default function Header() {
         <>
             <header>
                 <h1>Hello form header</h1>
-                <Link to="/">
-                    <span>Home</span> <br/>
-                </Link>
-                <Link to="/fir">
-                    <span>First page</span> <br />
-                </Link>
-                <Link to="/sec">
-                    <span>Second page</span> <br/>
-                </Link>
-                <Link to="/register">
-                    <span>Register</span> <br/>
-                </Link>
-                <Link to="/login">
-                    <span>Login</span> <br />
-                </Link>
-                <Link to="/profile">
-                    <span>Profile</span> <br/>
-                </Link>
+                <div className="head_container">
+                    <Link to="/">
+                        <span>Home</span> <br/>
+                    </Link>
+                    <Link to="/fir">
+                        <span>First page</span> <br />
+                    </Link>
+                    <Link to="/sec">
+                        <span>Second page</span> <br/>
+                    </Link>
+                    <Link to="/register">
+                        <span>Register</span> <br/>
+                    </Link>
+                    <Link to="/login">
+                        <span>Login</span> <br />
+                    </Link>
+                    <Link to="/profile">
+                        <span>Profile</span> <br/>
+                    </Link>
+                </div>
             </header>
         </>
     )
